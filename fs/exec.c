b2b3ab454b9bccfca1ed9c99b77a5ac2d173078c
Ghostess <72419159+Ghostess@users.noreply.github.com>
2026-06-13 18:45:41 +0000

    switched to manual hooks (kernelsu) cherry-picked some defconfig changes from https://github.com/Mesa-Labs-Archive/android_kernel_samsung_sm7325

--- fs/exec.c
+++ fs/exec.c
@@ -1932,6 +1932,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_MANUAL_HOOK)
+	__attribute__((hot))
+	extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+#endif
+
 int do_execve_file(struct file *file, void *__argv, void *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
@@ -1946,6 +1951,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+	#if defined(CONFIG_KSU) && defined(CONFIG_KSU_MANUAL_HOOK)
+		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
